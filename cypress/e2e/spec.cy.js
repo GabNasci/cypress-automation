79a832e4784b5473e8bfd77a783ffdc28299cdab
Gabriel Nascimento <gabriel.nascimento@grupointegrado.br>
2024-09-14 11:05:50 -0300

    Validação de campo e URL

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,19 +1,26 @@
-describe('Suíte de testes do google.', () => {
-  it('Deve acessar a página do google.', () => {
-    cy.visit('https://www.google.com')
+describe.only('Suíte de testes do integrado.', () => {
+
+  it('Deve visitar a página do integrado validar campos Área do Candidato e Inscreva-se e o Título', () => {
+    cy.visit('https://grupointegrado.br')
+    cy.title().should('eq', 'Início | Centro Universitário Integrado')
+    cy.contains('Área do candidato').should('exist')
+    cy.contains('Inscreva-se').should('exist')
   })
 
-  it('Deve visitar a página e validar o título.', () => {
-    cy.visit('https://www.google.com')
-    cy.title().should('eq', 'Google')
+  it('Deve clicar na opção Área do candidato e validar url', () => {
+    cy.visit('https://grupointegrado.br')
+    cy.contains('Área do candidato').should('exist').first().click()
+    cy.url().should('eq', 'https://www.grupointegrado.br/#area-candidato')
+
   })
-  
+
 })
 
-describe.only('Suíte de testes MageBit', () => {
+describe('Suíte de testes MageBit', () => {
   it('Deve acessar a página e validar o título.', () => {
     cy.visit('http://magento2-demo.magebit.com')
     cy.title().should('eq', 'Magento 2 Commerce (Enterprise) Demo - Magebit')
+
   })
 })
 
