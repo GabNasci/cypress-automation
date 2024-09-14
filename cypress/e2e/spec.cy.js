b2a549974bc3e9c7dd80fa99b8339c05d6876acb
Gabriel Nascimento <gabriel.nascimento@grupointegrado.br>
2024-09-14 10:32:13 -0300

    desenvolvendo teste de validação de título no site Mage bit

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,4 +2,18 @@ describe('Suíte de testes do google.', () => {
   it('Deve acessar a página do google.', () => {
     cy.visit('https://www.google.com')
   })
+
+  it('Deve visitar a página e validar o título.', () => {
+    cy.visit('https://www.google.com')
+    cy.title().should('eq', 'Google')
+  })
+  
 })
+
+describe.only('Suíte de testes MageBit', () => {
+  it('Deve acessar a página e validar o título.', () => {
+    cy.visit('http://magento2-demo.magebit.com')
+    cy.title().should('eq', 'Magento 2 Commerce (Enterprise) Demo - Magebit')
+  })
+})
+
